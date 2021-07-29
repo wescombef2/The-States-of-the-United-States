94161e36fe0f9bfcd9f00ee7cae6850ccb2bcd7d
wescombef2 <66758208+wescombef2@users.noreply.github.com>
2021-07-30 10:59:02 +1200

    SUS C1 Version 1.0
    Version 1.0 - Created Menu GUI Window with Basic Formatting

--- SUS_C1_Foundational_GUI.py
+++ SUS_C1_Foundational_GUI.py
@@ -1,8 +1,37 @@
 """
 The States of the United States
-Component 01 - Menu GUI
-Version 1.0 - Created Box and Frame, with Title and Headings
+Component 01 - Foundational GUI
+Version 1.0 - Created Menu GUI Window with Basic Formatting
 Finn Wescombe
-26/07/21
+29/07/21
 """
 
+# Import Tools
+from tkinter import * # For GUI Display
+from functools import partial # To Prevent Unwanted Windows
+import csv # For External States File and Saves File
+
+# Menu GUI Class
+class Menu:
+    # Initialize Function
+    def __init__(self):
+        # Define Background Colour
+        bg_colour = "light_grey"
+
+        # Create Main Frame
+        self.frm_m = Frame(width=100, height=200, bg=bg_colour)
+        self.frm_m.grid()
+
+        # Create Heading (Row 0)
+        self.lbl_m_heading = Label(self.frm_m,
+                                   text="The States of the United States",
+                                   font=("Arial", "16", "Bold"),
+                                   bg=bg_colour,
+                                   padx=10, pady=5)
+
+# Main Routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("The Presidential Election of the United States of America")
+    main_window = Menu()
+    root.mainloop()
