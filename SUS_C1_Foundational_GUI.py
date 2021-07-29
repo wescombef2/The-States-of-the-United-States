4ef807aae1e3bbafdc1bcd5020bdaeab11423187
wescombef2 <66758208+wescombef2@users.noreply.github.com>
2021-07-30 11:05:47 +1200

    SUS C1 Version 1.1
    Version 1.1 - Bugfix - Changed 'light_grey' bg_colour to 'grey', 'Bold' heading font to 'bold', and added 'self.lbl_m_heading.grid(row=0)'

--- SUS_C1_Foundational_GUI.py
+++ SUS_C1_Foundational_GUI.py
@@ -1,7 +1,8 @@
 """
 The States of the United States
 Component 01 - Foundational GUI
-Version 1.0 - Created Menu GUI Window with Basic Formatting
+Version 1.1 - Bugfix - Changed 'light_grey' bg_colour to 'grey',
+'Bold' heading font to 'bold', and added 'self.lbl_m_heading.grid(row=0)'
 Finn Wescombe
 29/07/21
 """
@@ -11,12 +12,13 @@ from tkinter import * # For GUI Display
 from functools import partial # To Prevent Unwanted Windows
 import csv # For External States File and Saves File
 
+
 # Menu GUI Class
 class Menu:
     # Initialize Function
     def __init__(self):
         # Define Background Colour
-        bg_colour = "light_grey"
+        bg_colour = "grey"
 
         # Create Main Frame
         self.frm_m = Frame(width=100, height=200, bg=bg_colour)
@@ -25,9 +27,11 @@ class Menu:
         # Create Heading (Row 0)
         self.lbl_m_heading = Label(self.frm_m,
                                    text="The States of the United States",
-                                   font=("Arial", "16", "Bold"),
+                                   font=("Arial", "16", "bold"),
                                    bg=bg_colour,
                                    padx=10, pady=5)
+        self.lbl_m_heading.grid(row=0)
+
 
 # Main Routine
 if __name__ == "__main__":
