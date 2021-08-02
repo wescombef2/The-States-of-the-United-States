c74fd91d885c067ad4e914aa6bd9af4e07884000
wescombef2 <66758208+wescombef2@users.noreply.github.com>
2021-08-03 10:58:14 +1200

    SUS C1 Version 5.0
    Version 5.0 - Added Game Window and Functionality Based off of Previous.

--- SUS_C1_Foundational_GUI.py
+++ SUS_C1_Foundational_GUI.py
@@ -1,7 +1,7 @@
 """
 The States of the United States
 Component 01 - Foundational GUI
-Version 4.0 - Added Cartogram Window and Functionality Based off of Instructions.
+Version 5.0 - Added Game Window and Functionality Based off of Previous.
 Finn Wescombe
 29/07/21
 """
@@ -39,11 +39,12 @@ class Menu:
         pixel_image = PhotoImage(width=1, height=1)
 
         # Play Game Button (Row 1 / Row 0)
-        self.btn_m_play = Button(self.frm_m_buttons,
+        self.btn_m_game = Button(self.frm_m_buttons,
                                  text="Play",
                                  width=15, height=2,
-                                 padx=1, pady=1)
-        self.btn_m_play.grid(row=0)
+                                 padx=1, pady=1,
+                                 command=self.fnc_get_g)
+        self.btn_m_game.grid(row=0)
 
         # View Cartogram Button (Row 1 / Row 1)
         self.btn_m_cartogram = Button(self.frm_m_buttons,
@@ -74,6 +75,9 @@ class Menu:
 
     def fnc_get_c(self):
         c = Cartogram(self)
+
+    def fnc_get_g(self):
+        g = Game(self)
 
 
 # Instructions GUI Class
@@ -182,6 +186,58 @@ class Cartogram:
         menu.btn_m_cartogram.configure(state=NORMAL)
         # Close Window
         self.box_c.destroy()
+
+
+# Cartogram GUI Class
+class Game:
+    # Initialize Function
+    def __init__(self, menu):
+        # Define Formatting Variables
+        bg_colour = "grey"
+
+        # Disable Button in Menu
+        menu.btn_m_game.configure(state=DISABLED)
+
+        # Create Window
+        self.box_g = Toplevel()
+        self.box_g.protocol('WM_DELETE_WINDOW', partial(self.fnc_g_close, menu))
+
+        # Main Frame
+        self.frm_g = Frame(self.box_g, width=100, height=100, bg=bg_colour)
+        self.frm_g.grid()
+
+        # Heading (Row 0)
+        self.lbl_g_heading = Label(self.frm_g,
+                                   text="Game",
+                                   font=("Arial", "16", "bold"),
+                                   bg=bg_colour,
+                                   padx=10, pady=5)
+        self.lbl_g_heading.grid(row=0)
+
+        # Cartogram Frame (Row 1)
+        self.frm_g_cartogram = Frame(self.frm_g, width=100, height=100, bg=bg_colour)
+        self.frm_g_cartogram.grid(row=2)
+
+        # Footer Frame (Row 2)
+        self.frm_g_footer = Frame(self.frm_g, width=100, height=20, bg=bg_colour)
+        self.frm_g_footer.grid(row=2)
+
+        # So that Width and Height is Measured in Pixels, Create 1x1 Image
+        pixel_image = PhotoImage(width=1, height=1)
+
+        # Close Button (Row 2 / Row 0)
+        self.btn_g_close = Button(self.frm_g_footer,
+                                 text="Close",
+                                 width=10, height=2,
+                                 padx=1, pady=1,
+                                 command=partial(self.fnc_g_close, menu))
+        self.btn_g_close.grid(row=0)
+
+    def fnc_g_close(self, menu):
+        # Re-enable Help Button
+        menu.btn_m_game.configure(state=NORMAL)
+        # Close Window
+        self.box_g.destroy()
 
 
 # Main Routine
